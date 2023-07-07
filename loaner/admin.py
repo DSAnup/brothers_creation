09b9170713a9e71a992cd411d49d746c06fb3a05
dsanup <anup12.m@gmail.com>
2023-07-07 13:27:02 +0600

    create loaner table & admin view

--- loaner/admin.py
+++ loaner/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
+from django.utils import timezone
+from django.contrib import messages
 
-# Register your models here.
+
+class LoanerAdmin(admin.ModelAdmin):
+    fields = [
+        ("userName", "password"),
+        ("firstName", "lastName"),
+        ("mobile", "isMember"),
+        ("address", "profilePic"),
+    ]
+    list_display = ("userName", "firstName", "mobile")
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            existing_obj = Loaner.objects.get(pk=obj.pk)
+            obj.DateCreated = existing_obj.DateCreated
+            obj.DateLastUpdated = timezone.now()
+            obj.CreatedBy = existing_obj.CreatedBy
+            obj.UpdatedBy = request.user.id
+            obj.save()
+        else:
+            if Loaner.objects.filter(userName=form.cleaned_data["userName"]).exists():
+                return messages.error(
+                    request, "The user name is already taken please choose another"
+                )
+            obj.CreatedBy = request.user.id
+            obj.save()
+
+
+admin.site.register(Loaner, LoanerAdmin)
--- loaner/models.py
+++ loaner/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Loaner(models.Model):
+    userName = models.CharField(max_length=50, verbose_name="User Name")
+    firstName = models.CharField(
+        max_length=80, blank=True, null=True, verbose_name="First Name"
+    )
+    lastName = models.CharField(
+        max_length=50, blank=True, null=True, verbose_name="Last Name"
+    )
+    mobile = models.CharField(max_length=20, verbose_name="Mobile Number")
+    address = models.CharField(
+        max_length=250, blank=True, null=True, verbose_name="Address"
+    )
+    password = models.CharField(max_length=100, blank=True, null=True, default="123456")
+    isActive = models.BooleanField(default=True, verbose_name="Is Active")
+    profilePic = models.FileField(upload_to="images/", null=True, blank=True)
+    CreatedBy = models.IntegerField(null=True, blank=True)
+    DateCreated = models.DateTimeField(default=timezone.now)
+    DateLastUpdated = models.DateTimeField(default=timezone.now)
+    UpdatedBy = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.firstName + self.lastName
