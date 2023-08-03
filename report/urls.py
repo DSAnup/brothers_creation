13702765b0bc19c28680815c8548495d8abea591
dsanup <anup12.m@gmail.com>
2023-08-03 12:24:33 +0600

    report loan installment

--- report/urls.py
+++ report/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="indexReport"),
     path("SISearch/", views.SISearch, name="SISearch"),
     path("SISearchResult/", views.SISearchResult, name="SISearchResult"),
+    path("SLSearch/", views.SLSearch, name="SLSearch"),
+    path("SLSearchResult/", views.SLSearchResult, name="SLSearchResult"),
 ]
